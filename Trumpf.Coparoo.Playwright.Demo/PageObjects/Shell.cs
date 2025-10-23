5b2621a0cc5694a74114f5383b576f0b0c49d990
Alexander Kaiser <Alexander.Kaiser@trumpf.com>
2025-10-23 23:27:35 +0200

    Add DynamicCompositionDemo and DemoTabObject classes to showcase modular page composition and dynamic registration of page objects

--- Trumpf.Coparoo.Playwright.Demo/PageObjects/Shell.cs
+++ Trumpf.Coparoo.Playwright.Demo/PageObjects/Shell.cs
@@ -1,11 +1,11 @@
 // Copyright 2016 - 2025 TRUMPF Werkzeugmaschinen GmbH + Co. KG.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,12 +22,12 @@ using Trumpf.Coparoo.Playwright.Demo.TabObjects;
 /// <summary>
 /// Page object representing the main application shell that contains the navigation menu.
 /// This page object is explicitly defined as a child of <see cref="DemoTab"/>.
-/// 
+///
 /// The Shell serves as the container for the entire application UI and provides
 /// access to the navigation menu. However, it does NOT have explicit knowledge of specific
 /// page implementations (Settings, Preferences) - these relationships are registered
 /// dynamically in the TabObject constructor.
-/// 
+///
 /// This separation enables teams to work independently on different sections of the application.
 /// </summary>
 public sealed class Shell : PageObject, IShell, IChildOf<DemoTab>
